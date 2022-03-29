1696366aac7779a4e0568ef314cb3639099932da
Jenni O'Connor <jenni@nextGenxr.com>
2022-03-29 17:38:40 -0500

    Cleanup header

--- Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h
+++ Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h
@@ -26,7 +26,7 @@
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f7xx.h"
+#include <stm32f7xx.h>
 #include "Legacy/stm32_hal_legacy.h"
 #include <stddef.h>
 
